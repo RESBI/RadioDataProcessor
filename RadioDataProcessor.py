f25d945f3340339797eb39e5b90baa94effe902d
Resbi <resbi@redstone.link>
2025-01-27 15:46:23 +0800

    add parallel processing
    Add parallel processing using multiprocessing.

--- RadioDataProcessor.py
+++ RadioDataProcessor.py
@@ -7,6 +7,8 @@
     2025/01/25 Resbi & RJGamesAhoy
 """
 
+import multiprocessing
+
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
@@ -72,38 +74,63 @@ def sortData(data):
     return [x[sortted_index], y[sortted_index]]
 
 
+def plotPlot(file, debug = 1):
+    prefix, index, file_name = file
+    read_out = readOne(file_name, debug = 1) 
+        
+    if (len(read_out)): 
+        header, data = read_out
+        #print(data)
+        # Fuck those weird noices
+        #data = filtOut(data, 0.05, debug = 1)
+        data_T = data.transpose()
+        #print(data_T)
+        data_sortted = sortData(data_T)
+        # plot them
+        print("[main] Saving to figure {}_plot_{}.png ...".format(prefix, index))
+        plt.plot(data_sortted[0], data_sortted[1], linewidth = 0.5) 
+        plt.xlabel("frequency / MHz")
+        plt.ylabel("power / dB")
+        plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter('%.6f'))
+        plt.subplots_adjust(left = 0.2)
+        plt.title(header[0])
+        plt.savefig("{}_plot_{}.png".format(prefix, index), dpi = 300)
+        plt.close()
+    else: 
+        print("[plotPlot] Can't draw plot for {} !".format(file_name))
+
+
+NUM_MP_PROCESS = 8
+
 if __name__ == "__main__": 
     data = np.array([1])
+    file_list = []
     index = 1
+    file_exist_flag = 1
     prefix = input("Please input file prefix ([prefix]_xxxx.txt): ")
     time_begin = time.time()
 
-    while (index): 
+    # Search files... 
+    while (file_exist_flag): 
         # data = [header, [f1, p1], [f2, p2], ...]
         file_name = "{}_{:04d}.txt".format(prefix, index)
-        read_out = readOne(file_name, debug = 1) 
+        try: 
+            file_try = open(file_name, "r") 
+            file_try.close() 
+        except:
+            file_exist_flag = 0
+            index -= 1
         
-        if (len(read_out)): 
-            header, data = read_out
-            #data = filtOut(data, 0.05, debug = 1) Originally used to filter out random noise spike at 1419.999876MHZ
-            data_T = data.transpose()
-            #print(data_T)
-            data_sortted = sortData(data_T)
-            # plot them
-            print("[main] Saving to figure {}_plot_{}.png ...".format(prefix, index))
-            plt.plot(data_sortted[0], data_sortted[1], linewidth = 0.5) 
-            plt.xlabel("frequency / MHz")
-            plt.ylabel("power / dB")
-            plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter('%.6f'))
-            plt.subplots_adjust(left = 0.2)
-            plt.title(header[0])
-            plt.savefig("{}_plot_{}.png".format(prefix, index), dpi = 300)
-            plt.close()
-        else: 
-            print("[main] Process finished!")
-            index = -1
-        
+        if (file_exist_flag): 
+            print("[main] Found: {} ...".format(file_name))
+            file_list.append([prefix, index, file_name])
+
         index += 1
+
+    print("[main] Found {} files in total.".format(index))
+
+    with multiprocessing.Pool(NUM_MP_PROCESS) as p: 
+        p.map(plotPlot, file_list)
 
     time_end = time.time() 
     print("[main] Cost {} seconds.".format(time_end - time_begin))
