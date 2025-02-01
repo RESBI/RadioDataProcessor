1ff1225681ad2f76171a4f91363c2d7dae95adb8
Resbi <resbi@redstone.link>
2025-02-01 16:25:47 +0800

    fixed timezone setting

--- skyChartGenerator.py
+++ skyChartGenerator.py
@@ -123,12 +123,13 @@ class skyChartGenerator():
         self.sendCommand('CLEANUPMAP')
         return
 
-    def setObservatory(self, latitude = "+42d55m42s", longitude="+85d32m50s", altitude="790", name = "United States - Michigan/Calvin_University", timezone = "EST"):
+    def setObservatory(self, latitude = "+42d55m42s", longitude="+85d32m50s", altitude="790", name = "United States - Michigan/Calvin_University", timezone = "UTC-5"):
         # I have no explanation to why this requires two identical commands to actually change the name of the observatory.
+        self.sendCommand(f'SETTZ {timezone}')
         self.sendCommand(f'SETOBS LAT:{latitude}LON:{longitude}ALT:{altitude}mOBS:{name}')
         self.sendCommand('CLEANUPMAP')
-        self.sendCommand(f'SETOBS LAT:{latitude}LON:{longitude}ALT:{altitude}mOBS:{name}')
         self.sendCommand(f'SETTZ {timezone}')
+        self.sendCommand(f'SETOBS LAT:{latitude}LON:{longitude}ALT:{altitude}mOBS:{name}')
         return
     
     def movePhotos(self, fileName, destination = 0):
