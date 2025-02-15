1b4afb7ccfb9b4c7ca8cce110b439fadf629d190
Admin <letterdief@gmail.com>
2025-02-15 22:02:51 +0100

    Applied some conventions for printing strings.

--- ImageMerger.py
+++ ImageMerger.py
@@ -1,3 +1,18 @@
+"""
+Script that merges images from a input folder.
+Example folder structure:
+    - /resources/output/Test1_chart_1.png
+    - /resources/output/Test1_chart_2.png
+    - /resources/output/Test1_plot_1.png
+    - /resources/output/Test1_plot_2.png
+Example command for script (if above is structure):
+ImageMerger.py
+  --input_directory=resources/output
+  --output_directory=resources/output/merged
+  --prefix_skychart=Test1_chart
+  --prefix_plot=Test1_plot
+"""
+
 import argparse
 import multiprocessing
 import os
@@ -5,58 +20,41 @@ import time
 
 from PIL import Image
 
-"""
-Script that merges images from a input folder.
-
-Example:
-
-If folder structure:
-    - /resources/output/Test1_chart_1.png
-    - /resources/output/Test1_chart_2.png
-    - /resources/output/Test1_plot_1.png
-    - /resources/output/Test1_plot_2.png
-
-Then command:
-
-ImageMerger.py 
-    --input_directory=resources/output
-    --output_directory=resources/output/merged 
-    --prefix_skychart=Test1_chart 
-    --prefix_plot=Test1_plot 
-"""
-
 
 def create_dir_if_not_exists(directory):
+    """Simple function for checking if there is directory and else create it."""
     if not os.path.isdir(directory):
         os.mkdir(directory)
-        print("Output directory created: %s" % directory)
+        print(f"Output directory created: {directory}")
 
 
 def merge_image(task):
+    """Function for merging images in multithreading environment."""
     both_paths_exists = False
-    skychart_path = "%s/%s_%d.png" % (task["input_dir"], task["skychart_prefix"], task["counter"])
-    plot_data_path = "%s/%s_%d.png" % (task["input_dir"], task["plot_prefix"], task["counter"])
+    skychart_path = f'{task["input_dir"]}/{task["skychart_prefix"]}_{task["counter"]}.png'
+    plot_data_path = f'{task["input_dir"]}/{task["plot_prefix"]}_{task["counter"]}.png'
 
     # Two if-statements for logging purposes.
     if os.path.exists(skychart_path):
         if os.path.exists(plot_data_path):
             both_paths_exists = True
         else:
-            print("%s does not exist!" % plot_data_path)
+            print(f"{plot_data_path} does not exist!")
     else:
-        print("%s does not exist!" % skychart_path)
+        print(f"{skychart_path} does not exist!")
 
     if both_paths_exists:
         create_dir_if_not_exists(task["output_dir"])
-        with (Image.open(plot_data_path) as plot_data_image, Image.open(skychart_path) as sky_chart_image):
+        with (Image.open(plot_data_path) as plot_data_image,
+              Image.open(skychart_path) as sky_chart_image):
             width = plot_data_image.width + sky_chart_image.width
             height = max(plot_data_image.height, sky_chart_image.height)
             merged_image = Image.new("RGB", (width, height))
             merged_image.paste(plot_data_image)
             merged_image.paste(sky_chart_image, (plot_data_image.size[0], 0))
-            new_image_path = "%s/plot_sky_merge_%d.jpeg" % (task["output_dir"], task["counter"])
+            new_image_path = f'{task["output_dir"]}/plot_sky_merge_{task["counter"]}.jpeg'
             merged_image.save(new_image_path, "JPEG")
-            print("Image created: %s" % new_image_path)
+            print(f"Image created: {new_image_path}")
 
 
 if __name__ == '__main__':
@@ -90,12 +88,12 @@ if __name__ == '__main__':
     plot_prefix = args.prefix_plot
 
     if os.path.exists(input_directory):
-        counter = 1
         fileCount = len(os.listdir(input_directory)) / 2
     else:
-        raise Exception("Input directory \"%s\" does not exist." % input_directory)
+        raise FileNotFoundError(f"Input directory \"{input_directory}\" does not exist.")
 
     multithreading_tasks = []
+    counter = 1
     while counter <= fileCount:
         multithreading_tasks.append({"input_dir": input_directory,
                                      "output_dir": output_directory,
@@ -108,4 +106,4 @@ if __name__ == '__main__':
         pool.map(merge_image, multithreading_tasks)
 
     runTime = time.time() - startTime
-    print("Time: %d minutes %d second" % (runTime // 60, runTime % 60))
+    print(f"Time: {runTime // 60} minutes {runTime % 60} second")
